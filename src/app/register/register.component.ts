b99484430a859b5b6c521e40ae2689152b2fca73
AlbertBarrachina <87174156+AlbertBarrachina@users.noreply.github.com>
2024-02-05 20:09:07 +0100

    5/2/2024 20:08h

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,6 +1,6 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Component } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
-import {User} from 'src/models/user.model';
 
 @Component({
   selector: 'app-register',
@@ -8,14 +8,34 @@ import {User} from 'src/models/user.model';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent {
-@Output() RegistrarEvent: EventEmitter<User> = new EventEmitter<User>();
   RegisterForm: FormGroup = new FormGroup({
     email: new FormControl('', [Validators.required, Validators.email]),
     password: new FormControl('', [Validators.required]),
     repeat_password: new FormControl('', [Validators.required])
   });
+  authService: any;
+  constructor(private http: HttpClient) {
 
-  Registrar(){
-    this.RegistrarEvent.emit(this.RegisterForm.value);
+  }
+  Registrar() {
+    const userData = this.RegisterForm.value;
+    const apiUrl = 'https://reques.in/api/register';
+
+    const headers = new HttpHeaders({
+      'Content-Type': 'application/json'
+    });
+
+
+    this.authService.registerUser(userData).subscribe(
+      (response: any) => {
+        console.log('Registration successful:', response);
+        // Handle success (optional)
+      },
+      (error: any) => {
+        console.error('Registration failed:', error);
+        // Handle error (optional)
+      }
+    );
+    return this.http.post(apiUrl, userData, { headers });
   }
 }
